4a118010c64b77fcfc4e0fed4147b6654547ad3f
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-07-17 23:51:12 +0900

    feat: 사용자 정보 확인(세션 정보)

--- src/main/java/com/example/springjwt/controller/MainController.java
+++ src/main/java/com/example/springjwt/controller/MainController.java
@@ -1,8 +1,14 @@
 package com.example.springjwt.controller;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.security.core.Authentication;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 // 웹서버로 동작하지 않고 api서버로 동작하기 때문에,
@@ -12,6 +18,17 @@ public class MainController {
 
     @GetMapping("/")
     public String mainP() {
-        return "main controller";
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        System.out.println("Main Controller : " + username + " : " + role);
+        return "Main Controller : " + username + " : " + role;
     }
 }
