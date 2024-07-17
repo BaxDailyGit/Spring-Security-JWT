45bd33606e39a18f287dbb94cfce94c40896e569
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-07-17 22:37:29 +0900

    feat: 로그인 실패시 401 응답 코드 반환

--- src/main/java/com/example/springjwt/jwt/LoginFilter.java
+++ src/main/java/com/example/springjwt/jwt/LoginFilter.java
@@ -75,7 +75,9 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     //로그인 실패시 실행하는 메소드
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
-        System.out.println("로그인 실패");
+
+        //로그인 실패시 401 응답 코드 반환
+        response.setStatus(401);
     }
 
 
