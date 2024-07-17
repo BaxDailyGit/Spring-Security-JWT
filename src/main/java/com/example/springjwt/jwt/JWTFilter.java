b9066159b220a7647948d5c9a29a4239f6d15b69
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-07-17 23:04:54 +0900

    feat: JWT 검증 필터 메소드 구현

---
+++ src/main/java/com/example/springjwt/jwt/JWTFilter.java
@@ -0,0 +1,75 @@
+package com.example.springjwt.jwt;
+
+import com.example.springjwt.dto.CustomUserDetails;
+import com.example.springjwt.entity.UserEntity;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+// JWTFilter는 클라이언트의 요청을 가로채서 JWT 토큰을 검증하는 역할을 한다.
+// 클라이언트의 요청에서 Authorization 헤더를 추출하여 토큰을 획득하고, 토큰을 검증한다.
+// 토큰이 유효하면 UserDetails 객체를 생성하여 AuthenticationManager에게 전달한다.
+// AuthenticationManager는 UserDetails 객체를 검증하고, 검증이 성공하면 SecurityContextHolder에 UserDetails 객체를 등록한다.
+// 이후 클라이언트의 요청을 처리할 때 UserDetails 객체를 사용하여 권한을 검증한다.
+public class JWTFilter extends OncePerRequestFilter {
+
+    private final JWTUtil jwtUtil;
+    public JWTFilter(JWTUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //request에서 Authorization 헤더를 찾음
+        String authorization= request.getHeader("Authorization");
+
+        //Authorization 헤더 검증
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+
+            System.out.println("token null");
+            filterChain.doFilter(request, response);
+
+            //조건이 해당되면 메소드 종료 (필수)
+            return;
+        }
+
+        System.out.println("authorization now");
+        //Bearer 부분 제거 후 순수 토큰만 획득
+        String token = authorization.split(" ")[1];
+
+        //토큰 소멸 시간 검증
+        if (jwtUtil.isExpired(token)) {
+
+            System.out.println("token expired");
+            filterChain.doFilter(request, response);
+
+            //조건이 해당되면 메소드 종료 (필수)
+            return;
+        }
+        //토큰에서 username과 role 획득
+        String username = jwtUtil.getUsername(token);
+        String role = jwtUtil.getRole(token);
+
+        //userEntity를 생성하여 값 set
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername(username);
+        userEntity.setPassword("temppassword");
+        userEntity.setRole(role);
+
+        //UserDetails에 회원 정보 객체 담기
+        CustomUserDetails customUserDetails = new CustomUserDetails(userEntity);
+
+        //스프링 시큐리티 인증 토큰 생성
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+        //세션에 사용자 등록
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
+    }
+}
